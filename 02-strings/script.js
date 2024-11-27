60dcadf099ecb5cfdcce7b3ba6f48fda99097f20
mattianatale <mattianatale07@gmail.com>
2024-11-27 15:38:21 +0100

    feat: strings add exercise 2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -10,6 +10,15 @@ console.log(name);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+let number = 7;
+console.log(number);
+
+let text = "Ciao";
+console.log(text);
+
+let bool = true;
+console.log(bool);
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
